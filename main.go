3ecf6d7967d989100a29953a3611bf91698856a8
Cristiano Colangelo <cristiano@helio.exchange>
2022-10-20 12:56:38 +0200

    Add graceful shutdown for Gin server

--- main.go
+++ main.go
@@ -1,13 +1,18 @@
 package main
 
 import (
+	"context"
 	"errors"
+	"fmt"
 	"github.com/gin-gonic/gin"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 	"gopkg.in/yaml.v3"
 	"log"
 	"net/http"
 	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/promauto"
@@ -23,6 +28,7 @@ type Config struct {
 }
 
 type TargetExporter struct {
+	srv    *http.Server
 	cfg    *Config
 	gauges []prometheus.Gauge
 }
@@ -32,15 +38,15 @@ var api TargetExporter
 func (t *TargetExporter) StartMetrics() {
 	go func() {
 		log.Println("Loading targets")
-		for nodeName, target := range api.cfg.Targets {
-			log.Println(nodeName)
+		for nodeName, target := range t.cfg.Targets {
+			log.Printf("target loaded: %s\n", nodeName)
 			currentGauge := promauto.NewGauge(prometheus.GaugeOpts{
-				Name:        api.cfg.TargetMetricName,
+				Name:        t.cfg.TargetMetricName,
 				ConstLabels: map[string]string{"instance": nodeName},
 			})
 
 			currentGauge.Set(target)
-			api.gauges = append(api.gauges, currentGauge)
+			t.gauges = append(t.gauges, currentGauge)
 		}
 
 		log.Println("Starting metrics server")
@@ -52,16 +58,23 @@ func (t *TargetExporter) StartMetrics() {
 func (t *TargetExporter) StartApi() {
 	go func() {
 		log.Println("Starting API server")
-		r := setupRoutes()
-		_ = r.Run(":8080")
+		t.srv = setupRoutes()
+		if err := t.srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatalf("listen: %s\n", err)
+		}
 	}()
 }
 
-func GetTargets(context *gin.Context) {
-	g.JSON(http.StatusOK, "hello world")
+func (t *TargetExporter) GetServer() *http.Server {
+	return t.srv
 }
 
-func setupRoutes() *gin.Engine {
+func GetTargets(g *gin.Context) {
+	time.Sleep(10 * time.Second)
+	g.String(http.StatusOK, "Welcome Gin Server")
+}
+
+func setupRoutes() *http.Server {
 	r := gin.Default()
 
 	v1 := r.Group("/api/v1")
@@ -69,7 +82,12 @@ func setupRoutes() *gin.Engine {
 		v1.GET("/targets", GetTargets)
 	}
 
-	return r
+	srv := &http.Server{
+		Addr:    ":8080",
+		Handler: r,
+	}
+
+	return srv
 }
 
 func init() {
@@ -92,8 +110,29 @@ func init() {
 }
 
 func main() {
+	ctx, stop := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
+	defer stop()
+
 	// TODO: Use gin to create the API
 	// TODO: Implement graceful shutdown using context
 	api.StartMetrics()
 	api.StartApi()
+
+	// Listen for the interrupt signal from the OS
+	<-ctx.Done()
+
+	// Restore default behavior on the interrupt signal and notify user of shutdown
+	stop()
+	log.Println("Shutting down gracefully, press Ctrl+C again to force")
+
+	// The context is used to inform the server it has 5 seconds to finish
+	// the request it is currently handling
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	fmt.Print("ping")
+	if err := api.GetServer().Shutdown(ctx); err != nil {
+		log.Fatal("Server forced to shutdown: ", err)
+	}
+
+	log.Println("Server exiting")
 }
