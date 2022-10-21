17acfb1386f148bc91be85695221ec75dfa432cd
Cristiano Colangelo <cristiano@helio.exchange>
2022-10-21 10:12:42 +0200

    Refactoring, add exporter package

--- main.go
+++ main.go
@@ -3,102 +3,21 @@ package main
 import (
 	"context"
 	"errors"
-	"github.com/gin-gonic/gin"
-	"github.com/prometheus/client_golang/prometheus/promauto"
-	"github.com/prometheus/client_golang/prometheus/promhttp"
 	"gopkg.in/yaml.v3"
 	"log"
-	"net/http"
 	"os"
 	"os/signal"
 	"syscall"
 	"time"
 
-	"github.com/prometheus/client_golang/prometheus"
+	. "git.helio.dev/eco-qube/target-exporter/pkg/exporter"
 )
 
 const (
 	ErrLoadingConfigFile = "error loading config file"
 )
 
-type Config struct {
-	TargetMetricName string             `yaml:"targetMetricName"`
-	Targets          map[string]float64 `yaml:"targets"`
-}
-
-type TargetExporter struct {
-	apiSrv     *http.Server
-	metricsSrv *http.Server
-	cfg        *Config
-	gauges     []prometheus.Gauge
-}
-
-var api TargetExporter
-
-func (t *TargetExporter) StartMetrics() {
-	log.Println("Loading targets")
-	for nodeName, target := range t.cfg.Targets {
-		log.Printf("target loaded: %s\n", nodeName)
-		currentGauge := promauto.NewGauge(prometheus.GaugeOpts{
-			Name:        t.cfg.TargetMetricName,
-			ConstLabels: map[string]string{"instance": nodeName},
-		})
-		currentGauge.Set(target)
-		t.gauges = append(t.gauges, currentGauge)
-	}
-	t.metricsSrv = &http.Server{
-		Addr:    ":2112",
-		Handler: promhttp.Handler(),
-	}
-
-	go func() {
-		log.Println("Starting metrics server")
-		if err := t.metricsSrv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
-			log.Fatalf("listen: %s\n", err)
-		}
-	}()
-}
-
-func (t *TargetExporter) StartApi() {
-	t.apiSrv = setupRoutes()
-
-	go func() {
-		log.Println("Starting API server")
-		if err := t.apiSrv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
-			log.Fatalf("listen: %s\n", err)
-		}
-	}()
-}
-
-func (t *TargetExporter) GetApiServer() *http.Server {
-	return t.apiSrv
-}
-
-func (t *TargetExporter) GetMetricsServer() *http.Server {
-	return t.metricsSrv
-}
-
-func GetTargets(g *gin.Context) {
-	g.JSON(200, gin.H{
-		"targets": api.cfg.Targets,
-	})
-}
-
-func setupRoutes() *http.Server {
-	r := gin.Default()
-
-	v1 := r.Group("/api/v1")
-	{
-		v1.GET("/targets", GetTargets)
-	}
-
-	srv := &http.Server{
-		Addr:    ":8080",
-		Handler: r,
-	}
-
-	return srv
-}
+var api *TargetExporter
 
 func init() {
 	if _, err := os.Stat("./config.yaml"); errors.Is(err, os.ErrNotExist) {
@@ -113,10 +32,7 @@ func init() {
 	if err != nil {
 		log.Fatalf("%s: %v", ErrLoadingConfigFile, err)
 	}
-	api = TargetExporter{
-		cfg:    &cfg,
-		gauges: []prometheus.Gauge{},
-	}
+	api = NewTargetExporter(&cfg)
 }
 
 func main() {
@@ -135,7 +51,7 @@ func main() {
 	stop()
 	log.Println("Shutting down gracefully, press Ctrl+C again to force")
 
-	// The context is used to inform the server it has 5 seconds to finish
+	// The context is used to inform the server it has 30 seconds to finish
 	// the request it is currently handling
 	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
 	defer cancel()
---
+++ pkg/exporter/exporter.go
@@ -0,0 +1,137 @@
+package exporter
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promauto"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+	"log"
+	"net/http"
+)
+
+type Config struct {
+	TargetMetricName string             `yaml:"targetMetricName"`
+	Targets          map[string]float64 `yaml:"targets"`
+}
+
+type Target struct {
+	target float64
+	gauge  prometheus.Gauge
+}
+
+func (api *Target) Set(target float64) {
+	api.gauge.Set(target)
+	api.target = target
+}
+
+func (api *Target) GetTarget() float64 {
+	return api.target
+}
+
+type TargetExporter struct {
+	apiSrv     *http.Server
+	metricsSrv *http.Server
+	bootCfg    *Config
+	targets    map[string]*Target
+}
+
+func NewTargetExporter(cfg *Config) *TargetExporter {
+	return &TargetExporter{
+		bootCfg: cfg,
+		targets: make(map[string]*Target),
+	}
+}
+
+func (t *TargetExporter) Targets() map[string]*Target {
+	return t.targets
+}
+
+func (t *TargetExporter) StartMetrics() {
+	log.Println("Loading targets")
+	for nodeName, target := range t.bootCfg.Targets {
+		log.Printf("target loaded: %s\n", nodeName)
+		currentGauge := promauto.NewGauge(prometheus.GaugeOpts{
+			Name:        t.bootCfg.TargetMetricName,
+			ConstLabels: map[string]string{"instance": nodeName},
+		})
+		currentGauge.Set(target)
+		t.targets[nodeName] = &Target{target, currentGauge}
+	}
+	t.metricsSrv = &http.Server{
+		Addr:    ":2112",
+		Handler: promhttp.Handler(),
+	}
+
+	go func() {
+		log.Println("Starting metrics server")
+		if err := t.metricsSrv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatalf("listen: %s\n", err)
+		}
+	}()
+}
+
+func (t *TargetExporter) StartApi() {
+	// Setup routes
+	r := gin.Default()
+	v1 := r.Group("/api/v1")
+	{
+		v1.GET("/targets", t.getTargetsResponse)
+		v1.POST("/targets", t.postTargetsRequest)
+	}
+	srv := &http.Server{
+		Addr:    ":8080",
+		Handler: r,
+	}
+	t.apiSrv = srv
+
+	go func() {
+		log.Println("Starting API server")
+		if err := t.apiSrv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatalf("listen: %s\n", err)
+		}
+	}()
+}
+
+func (t *TargetExporter) GetApiServer() *http.Server {
+	return t.apiSrv
+}
+
+func (t *TargetExporter) GetMetricsServer() *http.Server {
+	return t.metricsSrv
+}
+
+/************* API REQUESTS AND RESPONSE TYPES *************/
+
+type TargetsResponse struct {
+	Targets map[string]float64 `json:"targets"`
+}
+
+func (t *TargetExporter) getTargetsResponse(g *gin.Context) {
+	payload := TargetsResponse{Targets: make(map[string]float64)}
+	for node, target := range t.targets {
+		payload.Targets[node] = target.GetTarget()
+	}
+	g.JSON(200, gin.H{
+		"targets": payload,
+	})
+}
+
+type TargetsRequest struct {
+	Targets map[string]float64 `json:"targets"`
+}
+
+func (t *TargetExporter) postTargetsRequest(g *gin.Context) {
+	payload := TargetsRequest{}
+	if err := g.ShouldBindJSON(&payload); err != nil {
+		g.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+	for node, target := range payload.Targets {
+		if _, ok := t.Targets()[node]; ok {
+			t.Targets()[node].Set(target)
+		}
+	}
+	g.JSON(200, gin.H{
+		"message": "success",
+	})
+}
