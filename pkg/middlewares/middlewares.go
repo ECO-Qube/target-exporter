1d090d193748db5d286dca069191ae7cd0608e17
Cristiano Colangelo <cristiano@helio.exchange>
2022-10-27 16:09:06 +0200

    Improve CorsEnabledMiddleware

--- pkg/middlewares/middlewares.go
+++ pkg/middlewares/middlewares.go
@@ -4,7 +4,17 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+// from https://stackoverflow.com/questions/29418478/go-gin-framework-cors
 func CorsEnabledMiddleware(c *gin.Context) {
 	c.Header("Access-Control-Allow-Origin", "http://localhost:3000")
+	c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+	c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+	c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT")
+
+	if c.Request.Method == "OPTIONS" {
+		c.AbortWithStatus(204)
+		return
+	}
+
 	c.Next()
 }
