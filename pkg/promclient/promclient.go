0aceef8bb78131dda423501fcfae1bc905b6a82f
Cristiano Colangelo <cristiano@helio.exchange>
2022-12-06 17:54:25 +0100

    Add correct local kubeconfig
    Remember to not edit the server IP with sed as for the host machine!

--- pkg/promclient/promclient.go
+++ pkg/promclient/promclient.go
@@ -1,16 +1,16 @@
 package promclient
 
 import (
-  v1 "github.com/prometheus/client_golang/api/prometheus/v1"
-  "go.uber.org/zap"
+	v1 "github.com/prometheus/client_golang/api/prometheus/v1"
+	"go.uber.org/zap"
 )
 
 type Promclient struct {
-  v1.API
+	v1.API
 
-  logger *zap.Logger
+	logger *zap.Logger
 }
 
 func NewPromClient(client v1.API, logger *zap.Logger) *Promclient {
-  return &Promclient{client, logger}
+	return &Promclient{client, logger}
 }
