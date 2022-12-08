04aa9ffb11f00c5dd196f3ff906bc5cad38d7544
Cristiano Colangelo <cristiano@helio.exchange>
2022-12-08 11:28:28 +0100

    Trying to parse model.Value to proper type

--- pkg/promclient/promclient.go
+++ pkg/promclient/promclient.go
@@ -1,54 +1,50 @@
 package promclient
 
 import (
-  ctx "context"
-  "fmt"
-  v1 "github.com/prometheus/client_golang/api/prometheus/v1"
-  "go.uber.org/zap"
-  "os"
-  "time"
+	ctx "context"
+	"fmt"
+	v1 "github.com/prometheus/client_golang/api/prometheus/v1"
+	"github.com/prometheus/common/model"
+	"go.uber.org/zap"
+	"time"
 )
 
 type Promclient struct {
-  v1.API
+	v1.API
 
-  logger *zap.Logger
+	logger *zap.Logger
 }
+
+const CpuNodeQuery = `100 - 100 * avg by (instance) (rate(node_cpu_seconds_total{mode="idle"}[1m]))`
 
 func NewPromClient(client v1.API, logger *zap.Logger) *Promclient {
-  return &Promclient{client, logger}
+	return &Promclient{client, logger}
 }
 
-type QueryResult struct {
-  Data struct {
-    ResultType string `json:"resultType"`
-    Result     []struct {
-      Metric struct {
-        __name__ string `json:"__name__"`
-        job      string `json:"job"`
-        instance string `json:"instance"`
-      } `json:"metric"`
-      Value []interface{} `json:"value"`
-    } `json:"result"`
-  } `json:"data"`
-}
+func (p *Promclient) GetCurrentCpuUsagePerNode() error {
+	r := v1.Range{
+		Start: time.Now(),
+		End:   time.Now().Add(time.Second),
+		Step:  time.Minute,
+	}
+	result, warnings, err := p.QueryRange(ctx.Background(),
+		CpuNodeQuery,
+		r,
+		v1.WithTimeout(5*time.Second))
+	if err != nil {
+		return err
+	}
+	if len(warnings) > 0 {
+		p.logger.Warn(fmt.Sprintf("Prometheus Warnings: %v\n", warnings))
+	}
 
-func (p *Promclient) GetCurrentCpuUsagePerNode() {
-  r := v1.Range{
-    Start: time.Now(),
-    End:   time.Now().Add(time.Second),
-    Step:  time.Minute,
-  }
-  result, warnings, err := p.QueryRange(ctx.Background(),
-    "100 - 100 * avg by (instance) (rate(node_cpu_seconds_total{mode=\"idle\"}[1m]))",
-    r,
-    v1.WithTimeout(5*time.Second))
-  if err != nil {
-    fmt.Printf("Error querying Prometheus: %v\n", err)
-    os.Exit(1)
-  }
-  if len(warnings) > 0 {
-    fmt.Printf("Warnings: %v\n", warnings)
-  }
-  fmt.Printf("Result:\n%v\n", result)
+	mapData := make(map[model.Time]model.SampleValue)
+
+	for _, val := range result.(model.Matrix)[0].Values {
+		mapData[val.Timestamp] = val.Value
+	}
+
+	fmt.Printf("Result:\n%v\n", len(mapData))
+
+	return nil
 }
