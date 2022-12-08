dbac446a62092cc4d0fe1ed3c2eb029276f011c4
Cristiano Colangelo <cristiano@helio.exchange>
2022-12-08 13:22:02 +0100

    Finish prometheus query cpu usage per node

--- pkg/promclient/promclient.go
+++ pkg/promclient/promclient.go
@@ -6,8 +6,12 @@ import (
 	v1 "github.com/prometheus/client_golang/api/prometheus/v1"
 	"github.com/prometheus/common/model"
 	"go.uber.org/zap"
+	"strconv"
+	"strings"
 	"time"
 )
+
+const nodeCpuPromQuery = `100 - 100 * avg by (instance) (rate(node_cpu_seconds_total{mode="idle"}[1m]))`
 
 type Promclient struct {
 	v1.API
@@ -15,36 +19,49 @@ type Promclient struct {
 	logger *zap.Logger
 }
 
-const CpuNodeQuery = `100 - 100 * avg by (instance) (rate(node_cpu_seconds_total{mode="idle"}[1m]))`
+type NodeCpuUsage struct {
+	timestamp time.Time
+	usage     float64 // CPU usage in percentage 0-100
+}
 
 func NewPromClient(client v1.API, logger *zap.Logger) *Promclient {
 	return &Promclient{client, logger}
 }
 
-func (p *Promclient) GetCurrentCpuUsagePerNode() error {
+func (p *Promclient) GetCurrentCpuUsagePerNode() ([]NodeCpuUsage, error) {
 	r := v1.Range{
 		Start: time.Now(),
 		End:   time.Now().Add(time.Second),
 		Step:  time.Minute,
 	}
 	result, warnings, err := p.QueryRange(ctx.Background(),
-		CpuNodeQuery,
+		nodeCpuPromQuery,
 		r,
 		v1.WithTimeout(5*time.Second))
 	if err != nil {
-		return err
+		return nil, err
 	}
 	if len(warnings) > 0 {
 		p.logger.Warn(fmt.Sprintf("Prometheus Warnings: %v\n", warnings))
 	}
 
-	mapData := make(map[model.Time]model.SampleValue)
+	cpuUsagesPerNode := make([]NodeCpuUsage, 0)
 
-	for _, val := range result.(model.Matrix)[0].Values {
-		mapData[val.Timestamp] = val.Value
+	for _, entry := range result.(model.Matrix) {
+		fmt.Println(model.LabelSet(entry.Metric)["instance"])
+		// Assume there is only one value per each node (hence Values[0])
+		// Drop "fraction of second" from timestamp
+		currentValue := entry.Values[0]
+		ts, err := strconv.ParseInt(strings.Split(currentValue.Timestamp.String(), ".")[0], 10, 64)
+		if err != nil {
+			return nil, err
+		}
+		usage, err := strconv.ParseFloat(currentValue.Value.String(), 64)
+		cpuUsagesPerNode = append(cpuUsagesPerNode, NodeCpuUsage{
+			timestamp: time.Unix(ts, 0),
+			usage:     usage,
+		})
 	}
 
-	fmt.Printf("Result:\n%v\n", len(mapData))
-
-	return nil
+	return cpuUsagesPerNode, nil
 }
